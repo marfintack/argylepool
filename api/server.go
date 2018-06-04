4c4ec136286d3a2d92b94f56784a90bbd7397648
marfintack <root@APPLEs-MacBook-Pro-2.local>
2018-06-04 16:14:56 +0500

    altrating miner model

--- api/server.go
+++ api/server.go
@@ -150,7 +150,7 @@ func (s *ApiServer) collectStats() {
 		log.Fatal("Could not connect database %s", err)
 	}
 	fmt.Println("Connected to DB Successfully %s")
-	Miner := models.Miner{MinerAddress: "0xd4525b963d14995722aea0af936e00c0d9fa9820", BlockNumber: 75000, Reward: 5}
+	Miner := models.MinerDetail{MinerAddress: "0xd4525b963d14995722aea0af936e00c0d9fa9820", BlockNumber: 75000, Reward: 5}
 	db.Save(&Miner)
 	start := time.Now()
 	stats, err := s.backend.CollectStats(s.hashrateWindow, s.config.Blocks, s.config.Payments)
